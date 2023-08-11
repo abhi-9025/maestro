e34426245bade66efce42bb2dd15305a3ec7d052
Abhishek Bharti <abhishek@Abhisheks-MacBook-Air.local>
2023-08-11 20:30:30 +0530

    just for check

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
+  const [isMounted,setIsMounted]=useState(false)
+    useEffect(()=>{
+        setIsMounted(true)
+    },[])
+    if(!isMounted) return null
   return (
     <ClerkProvider>
       <html lang="en">
